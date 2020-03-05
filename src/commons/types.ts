824b45743a8928378a25dbebed8c3afea9214b1e
jonas <Jonasnepomuceno08@gmail.com>
2020-03-05 14:45:41 +0800

    add resolvers and typeDefs Folders

---
+++ src/commons/types.ts
@@ -0,0 +1,7 @@
+export interface IDBModel<T> {
+  insert: (input: any) => PromiseLike<T>;
+  getById: (input: any) => PromiseLike<T>;
+  getAll: () => PromiseLike<T>;
+  deleteById: (input: any) => PromiseLike<T>;
+  updateById: (input: any) => PromiseLike<T>;
+}
